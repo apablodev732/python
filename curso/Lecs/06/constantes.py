b4c1836423bda8c168608f7abac07316b1d16c31
apablo <ap.alonso3086@gmail.com>
2024-08-03 21:21:17 -0600

    03_08_24

--- curso/Lecs/06/constantes.py
+++
@@ -1,4 +0,0 @@
-MI_CONSTANTE = 'VaLOR DE MI CONSTANTE'
-
-class Matematicas:
-    PI = 3.1416
--- curso/Lecs/06/test_constantes.py
+++
@@ -1,9 +0,0 @@
-from constantes import *
-
-print(MI_CONSTANTE)
-print(Matematicas.PI);
-MI_CONSTANTE = 'Nuevo valor constante'
-print(MI_CONSTANTE)
-
-
-
